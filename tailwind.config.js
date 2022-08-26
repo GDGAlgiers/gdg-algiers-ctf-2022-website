5c365c6ec899b49b6e3a1bf54c08a70c88bc546c
Brivan-26 <ma.rezazi@esi-sba.dz>
2022-08-26 11:42:31 +0100

    Fixing tailwind import + adding screens config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
 module.exports = {
+    content: [
+        './src/pages/**/*.{js,jsx,ts,tsx}',
+        './src/components/**/*.{js,jsx,ts,tsx}',
+    ],
     theme: {
+        screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+            '2xl': '1536px',
+        },
         colors: {
             black: {
                 lighter: '#212121',

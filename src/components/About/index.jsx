65b2863a3130a5aee568c26ecb983ce001d31b12
Brivan-26 <ma.rezazi@esi-sba.dz>
2022-08-27 17:35:24 +0100

    Updated event's description + d-day countdown

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -9,10 +9,10 @@ const About = () => {
             <p
                 className={`${containerStyles.textShadow} text-center text-[22px] leading-[48px] px-[20px] md:text-[30px] md:leading-[86px] md:px-[96px] lg:text-[40px] lg:leading-[96px] lg:px-[164px] 2xl:text-[64px] 2xl:px-[245px] 2xl:leading-[144px]`}
             >
-                thank to this wonderful community, I&apos;m so excited to work
-                and share new leads projects, and ambitions... whether I&apos;m
-                a UX UI comanager or not, GDG will remain my family and
-                we&apos;ll do a lot of cool things that matter
+                Participate in the first edition of the cybersecurity
+                competition GDG Algiers CTF, and advance your cybersecurity
+                abilities with ambitious competitors from around the world, make
+                sure to mark your calendar from 7th to 9th of October.
             </p>
         </section>
     )

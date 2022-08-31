8a827456dda9823fe413744a311f1cdd8b9fae6f
Abdelkarim <la_bengherbia@esi.dz>
2022-08-31 14:21:55 +0100

    faq changes

--- src/components/FaQ/index.jsx
+++ src/components/FaQ/index.jsx
@@ -13,26 +13,30 @@ const FaQ = () => {
         setSelected(i)
     }
     return (
-        <section className='pt-96' id="faq">
+        <section id="faq">
             <p
-                className={`${containerStyles.textShadow} text-center text-[22px] leading-[48px] px-[20px] md:text-[30px] md:leading-[86px] md:px-[96px] lg:text-[40px] lg:leading-[96px] lg:px-[164px] 2xl:text-[64px] 2xl:px-[245px] 2xl:leading-[144px]`}
+                className={`${containerStyles.textShadow} text-center text-[18px] leading-[38px] md:text-[48px] md:leading-[96px] 2xl:text-[72px] 2xl:leading-[144px]`}
             >
                 FaQ
             </p>
-            <div className='flex  justify-center align-center'>
-                <div className='w-[74vw]'>
+            <p
+                className="w-[74vw] m-auto text-center text-[22px] leading-[21px] py-[20px] md:text-[21px] md:leading-[54px] md:py-[54px] lg:text-[21px] lg:leading-[54px] lg:px-[54px] 2xl:text-[32px] 2xl:py-[80px] 2xl:leading-[80px] text-[#FFFFFF]">
+                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem vivamus tortor id ultrices amet, 
+            </p>
+            <div className="flex justify-center align-center">
+                <div className="w-[74vw]">
                     {data.map((item , i) => (
                             <div>
                                 <svg viewBox="0 0 1704 63" fill="none" >
                                     <path d="M2 31.9999C509.5 104.5 990.5 -64.9999 1702 31.9999V62.9999H2V31.9999Z" fill="#1E1E1E"/>
                                     <path d="M1.5 32C537.612 103.388 953.151 -64.625 1702.5 31.9999M1703 30.9999V62.9998M1 31.0001V63.0001" stroke="#FB000A" stroke-width="3.5" stroke-linejoin="round"/>
                                 </svg>
-                                <div className='bg-[#1E1E1E] px-[5vw] py-[1.5vh] border-[0.1px] md:border-2 border-[#FB000A] border-t-0 border-b-0 md:border-t-0 md:border-b-0'>
-                                    <div className='flex justify-between items-center ' >
-                                        <h2 className='text-[14px] lg:text-[21px] 2xl:text-[32px]'>{item.question}</h2>
+                                <div className="bg-[#1E1E1E] px-[5vw] py-[8px] border-[0.1px] md:border-2 border-[#FB000A] border-t-0 border-b-0 md:border-t-0 md:border-b-0">
+                                    <div className="flex justify-between items-center " >
+                                        <h2 className="text-[14px] md:text-[21px] 2xl:text-[32px] font-Gilroy text-[#FFFFFF]">{item.question}</h2>
                                         {Selected == i ? <img src={hide} className="w-[4vw] h-auto cursor-pointer" onClick={() => {toggle(i)}} /> : <img src={show} className="w-[4vw] h-auto cursor-pointer" onClick={() => {toggle(i)}} />}
                                     </div>
-                                    <div className={Selected == i ? 'h-auto pt-4 text-[12px] lg:text-[16px] 2xl:text-[24px]' : 'max-h-0 overflow-hidden '}>
+                                    <div className={Selected == i ? "h-auto pt-[16px] md:pt-[21px] 2xl:pt-[32px] text-[10px] leading-[12px] md:text-[16px] md:leading-[32px] 2xl:text-[24px] 2xl:leading-[48px] font-Gilroy text-[#FFFFFF]" : "max-h-0 overflow-hidden "}>
                                     {item.answer}
                                     </div>
                                 </div>

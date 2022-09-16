129e3fe01df0218881c9991e0145b82b1036253d
akram09 <a.boutouchent@esi-sba.dz>
2022-09-16 23:09:32 +0100

    :sparkles: adding pentesterlab sponsor

--- src/components/Sponsors/index.jsx
+++ src/components/Sponsors/index.jsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import gcp from '../../images/gcp-logo.png'
 import rootMe from '../../images/rootme.svg'
 import digitalOcean from '../../images/digital-ocean.png'
+import pentesterLab from '../../images/pentesterlab.png'
 import Title from '../shared/Title'
 import Description from '../shared/Description'
 import Wall from '../../images/wall_footer.png'
@@ -17,7 +18,7 @@ const Sponsors = () => {
                 paragraph="
              The CTF was not possible without the contribution of our amzing sponsors. 
              Thanks for Google for sponsoring the CTF infrastructure, 
-             Thanks also to Digital Ocean and Root-Me for sponsoring the prizes of the CTF                 
+             Thanks also to Digital Ocean, PentesterLab and Root-Me for sponsoring the prizes of the CTF                 
             "
             />
             <div className="flex justify-center">
@@ -25,17 +26,22 @@ const Sponsors = () => {
                     <div className="flex justify-between items-center">
                         <img
                             src={gcp}
-                            className=" h-24 w-32 md:h-52 md:w-64 lg:h-72 lg:w-96 md:mx-3 lg:mx-8"
+                            className=" h-24 w-32 md:h-52 md:w-64 lg:h-56 lg:w-72 md:mx-3 lg:mx-8"
                             alt="Google Cloud Platform"
                         />
                         <img
                             src={digitalOcean}
-                            className=" h-20 w-20 md:h-32 md:w-32 lg:h-48 lg:w-48 md:mx-3  lg:mx-8"
+                            className=" h-20 w-20 md:h-32 md:w-32 lg:h-36 lg:w-36 md:mx-3  lg:mx-8"
                             alt="Digital Ocean"
                         />
                         <img
+                            src={pentesterLab}
+                            className=" h-20 w-32 md:h-24 md:w-64 lg:h-24 lg:w-72 md:mx-3 lg:mx-8"
+                            alt="PentesterLab"
+                        />
+                        <img
                             src={rootMe}
-                            className=" h-24 w-24 md:h-44 md:w-44 lg:h-64 lg:w-64 md:mx-3 lg:mx-8"
+                            className=" h-24 w-24 md:h-44 md:w-44 lg:h-48 lg:w-48 md:mx-3 lg:mx-8"
                             alt="Root-Me"
                         />
                     </div>
